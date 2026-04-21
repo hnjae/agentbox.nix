4480d9adf115c95c31d279b5ee96f8c4a5f48441
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-04-21 05:07:59 +0000

    feat: add library-backed command dispatch
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ src/commands/mod.rs
@@ -0,0 +1,22 @@
+// Copyright 2026 KIM Hyunjae
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::cli::Command;
+use crate::error::{Error, Result};
+
+pub fn dispatch(command: Command) -> Result<()> {
+    tracing::debug!(?command, "dispatching CLI command");
+
+    match command {
+        Command::Run(_) => Err(Error::not_yet_implemented("run")),
+        Command::Attach(_) => Err(Error::not_yet_implemented("attach")),
+        Command::Ls => Err(Error::not_yet_implemented("ls")),
+        Command::Rm(_) => Err(Error::not_yet_implemented("rm")),
+        Command::Completion => Err(Error::not_yet_implemented("completion")),
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+// Copyright 2026 KIM Hyunjae
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use std::ffi::OsString;
+use std::process::ExitCode;
+
+use clap::Parser;
+
+pub mod cli;
+pub mod commands;
+pub mod error;
+
+use cli::Cli;
+pub use error::{Error, Result};
+
+pub fn main() -> ExitCode {
+    match try_main() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(Error::Cli(error)) => error.exit(),
+        Err(error) => {
+            eprintln!("{error}");
+            ExitCode::FAILURE
+        }
+    }
+}
+
+pub fn try_main() -> Result<()> {
+    run(std::env::args_os())
+}
+
+pub fn run<I, T>(args: I) -> Result<()>
+where
+    I: IntoIterator<Item = T>,
+    T: Into<OsString> + Clone,
+{
+    let cli = Cli::try_parse_from(args)?;
+    commands::dispatch(cli.command)
+}
